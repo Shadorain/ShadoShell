163e690bc9289e3a361072d10cb3de57599f2aa7
Jordan Ferraris <jordanf7517@gmail.com>
2020-11-06 21:26:46 -0500

    quick change

--- shadosh.c
+++ shadosh.c
@@ -92,7 +92,7 @@ int main () { // int argc, char* argv[]) {
                 //shadofree();
                 exit(0);
             case ARGS:
-                break;
+                printf("ARGS\n");
         }
     /* } */
 
